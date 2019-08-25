ca2a44027a8fa92a2628c3725297a428f8fbc1ef
zhangshuaicai <zhangshuaicai6@163.com>
2019-08-25 11:43:45 +0800

    初始化代码

---
+++ src/com/atguigu/test/EgitDemo.java
@@ -0,0 +1,11 @@
+package com.atguigu.test;
+
+public class EgitDemo {
+
+	public static void main(String[] args) {
+		System.out.println(111);
+		System.out.println(222);
+		System.out.println(333);
+	}
+
+}
